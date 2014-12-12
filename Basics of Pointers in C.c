ce437284eb13ea789bd21e095179a5805c6cafb6
Alim Ul Karim <auk.junk@live.com>
2014-12-12 12:21:57 +0600

    Basic Pointers
    Basic Pointers

---
+++ Basics of Pointers in C.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<math.h>
+#include <conio.h> // for getch library files
+
+/*------------------------------------------------
+ *------------------------------------------------
+ * |name           :   Alim Ul Karim
+ * |email          :   alim@developers-organism.com
+ * |works          :   Developers Organism
+ * |blog           :   http://bit.ly/auk-blog
+ * |linkedin       :   http://linkd.in/alim-ul-karim
+ *------------------------------------------------
+ *------------------------------------------------
+ */
+
+//Write Functions Above
+
+int main(){
+    int     x;
+    int     a,b;
+
+    int *pointer = (int *)malloc(10 * sizeof(int)) ; // null pointer takes two bytes
+    // 0 , 1 . ..
+    // let's pointer is pointing to RAM address 500
+    //
+
+    printf("&pointer: %d , pointer: %d\n" , &pointer, pointer );
+    printf("pointing index of 0(item 1): %d\n" , (pointer+0));
+    printf("pointing index of 1(item 2): %d\n" , (pointer+1));
+
+
+
+    getch();
+    return 0;
+}
